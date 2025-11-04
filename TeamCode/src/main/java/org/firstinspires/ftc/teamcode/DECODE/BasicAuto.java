01681b9af1123cdd0cceb60e7d15ffc4d087387f
evann29 <evannewcomb29@gmail.com>
2025-11-03 20:37:57 -0500

    basic autonomous - still need to test
    also modified outtake subsystem to include midtake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE/BasicAuto.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.DECODE;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.EvanTeleOp.Drivetrain;
+
+public class BasicAuto extends LinearOpMode {
+    public Drivetrain drivetrain = new Drivetrain();
+    public In_take intake;
+    public Out_take outtake;
+    ElapsedTime timer = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        drivetrain.init(hardwareMap);
+        intake = new In_take(hardwareMap);
+        outtake = new Out_take(hardwareMap);
+
+        waitForStart();
+
+        timer.reset();
+        while(timer.seconds() < 1.5) {
+            drivetrain.moveRobot(0, -0.5, 0);
+        }
+        drivetrain.moveRobot(0,0,0);
+        timer.reset();
+        while(timer.seconds() < 0.5) {
+            outtake.basicMovement(1);
+        }
+        timer.reset();
+        while(timer.seconds() < 3) {
+            intake.movement(0.25);
+            outtake.setMidtake(0.25);
+        }
+        intake.movement(0);
+        outtake.setMidtake(0);
+        outtake.basicMovement(0);
+        timer.reset();
+        while(timer.seconds() < 1.5) {
+            drivetrain.moveRobot(0.5, 0, 0);
+        }
+        drivetrain.moveRobot(0,0,0);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE/Out_take.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE/Out_take.java
@@ -7,17 +7,24 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Out_take extends SubsystemBase {
     DcMotor out_take;
     DcMotor out_take2;
+    DcMotor midtake;
 
     public Out_take(HardwareMap hardware){
         out_take = hardware.get(DcMotor.class, "out_take");
         out_take2 = hardware.get(DcMotor.class,"out_take2");
+        midtake = hardware.get(DcMotor.class, "midtake");
 
         out_take.setDirection(DcMotor.Direction.REVERSE);
         out_take2.setDirection(DcMotor.Direction.REVERSE);
+        midtake.setDirection(DcMotor.Direction.REVERSE);
     }
     public void basicMovement (double power){
 
         out_take.setPower(-power);
         out_take2.setPower(power);
+    }
+
+    public void setMidtake(double power) {
+        midtake.setPower(power);
     }
 }

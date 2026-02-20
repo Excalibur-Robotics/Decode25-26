8960c17240a01ccaa1b3a56558c33a8f4bef6ae8
Vishwanth-B <vishwanthbolisetty@gmail.com>
2026-02-20 16:06:55 -0500

    Create LEDs.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V2/Subsystems/LEDs.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.V2.Subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.LED;
+
+public class LEDs extends SubsystemBase {
+    LED green;
+    LED red;
+    LED purple;
+
+    public LEDs (HardwareMap hardware){
+        green = hardware.get(LED.class,"GreenLED");
+        red = hardware.get(LED.class,"RedLED");
+        purple = hardware.get(LED.class,"purpleLED");
+    }
+
+    public void green() {
+        green.on();
+        red.off();
+        purple.off();
+    }
+
+    public void red(){
+        green.off();
+        red.on();
+        purple.off();
+    }
+
+    public void purple(){
+        green.off();
+        red.off();
+        purple.on();
+    }
+
+}
+
+

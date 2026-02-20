1c39bc437df6f99c669e206c5c38c10caf250dde
Vishwanth-B <vishwanthbolisetty@gmail.com>
2026-02-20 14:36:39 -0500

    Update States_TeleOP.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V2/TeleOp/States_TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V2/TeleOp/States_TeleOP.java
@@ -1,7 +1,24 @@
 package org.firstinspires.ftc.teamcode.V2.TeleOp;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.V2.Subsystems.DrivetrainSubsystem;
+import org.firstinspires.ftc.teamcode.V2.Subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.V2.Subsystems.OuttakeSubsystem;
+import org.firstinspires.ftc.teamcode.V2.Subsystems.SpindexerSubsystem;
 
 public class States_TeleOP extends CommandOpMode {
-    
+    IntakeSubsystem intake;
+    SpindexerSubsystem spindexer;
+    OuttakeSubsystem outtake;
+    DrivetrainSubsystem drivetrain;
+
+    public void initialize(){
+        intake = new IntakeSubsystem(hardwareMap);
+        spindexer= new SpindexerSubsystem(hardwareMap);
+        outtake = new OuttakeSubsystem(hardwareMap);
+        drivetrain = new DrivetrainSubsystem(hardwareMap);
+
+    }
 }

b8b6196ceae3e5c4f642b8ea9bdd201d4787a6d9
Vishwanth-B <vishwanthbolisetty@gmail.com>
2026-02-21 14:09:28 -0500

    Roundtwoiteration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V2/Subsystems/LEDs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V2/Subsystems/LEDs.java
@@ -3,40 +3,34 @@ package org.firstinspires.ftc.teamcode.V2.Subsystems;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.LED;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class LEDs extends SubsystemBase {
+    /*
     LED green;
     LED red;
     LED purple;
+*/
+    Servo led_light;
 
     public LEDs (HardwareMap hardware){
-        green = hardware.get(LED.class,"GreenLED");
-        //red = hardware.get(LED.class,"RedLED");
-        //purple = hardware.get(LED.class,"purpleLED");
+        led_light = hardware.get(Servo.class,"GreenLED")
     }
 
     public void green() {
-        green.on();
-        //red.off();
-        //purple.off();
+        led_light.setPosition(0.2);
     }
 
     public void red(){
-        green.off();
-        //red.on();
-        //purple.off();
+
     }
 
     public void purple(){
-        green.off();
-        //red.off();
-        //purple.on();
+
     }
 
     public void off() {
-        green.off();
-        red.off();
-        purple.off();
+
     }
 
 }

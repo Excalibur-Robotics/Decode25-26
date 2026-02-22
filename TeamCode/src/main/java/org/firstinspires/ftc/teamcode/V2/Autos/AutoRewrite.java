dcf57d7c9ef0731563a6390c9d81dbfafae06cb0
Vishwanth-B <vishwanthbolisetty@gmail.com>
2026-02-21 20:23:15 -0500

    Create AutoRewrite.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V2/Autos/AutoRewrite.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.V2.Autos;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous
+public class AutoRewrite extends CommandOpMode {
+
+}
